46619f495a2fb7dfd4ef655d93951cdda67ef8a0
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-10-10 17:51:37 +0530

    BE - added proxy and added CORS and fetching products from backend

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,23 @@ const app = express();
 // Serve static files from the 'assets' folder
 // app.use('/images', express.static('../frontend/src/assets/images'));
 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200); // Handle preflight requests by sending HTTP 200
+    }
+    next();
+  });
+
+
 // Routers : 
 app.use(routers);
 app.use('/api/products', productRoutes); 
