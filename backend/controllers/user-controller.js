cb9339fc7fe31dbf3dc948265fb74832874b5177
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2024-11-04 15:33:59 +0530

    BE - user mail and password validation

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,8 +1,28 @@
 const User = require('../models/userModel');
+const bcrypt = require("bcryptjs");
+const errorModel = require('../models/error-model');
 
 const authUser = async (req, res, next) => {
-    res.send("Auth User !!!!")
-}
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (!existingUser) {
+            return next(errorModel(401, "User not found, could not log you in."));
+        }
+
+        const isValidPassword = await bcrypt.compare(password, existingUser.password);
+
+        if (!isValidPassword) {
+            return next(errorModel(401, "Invalid credentials, could not log you in."));
+        }
+
+        res.json({ userId: existingUser.id, email: existingUser.email });
+    } catch (err) {
+        return next(errorModel(500, "Login failed, please try again later."));
+    }
+};
 
 const registerUser = async (req, res, next) => {
     res.send("Register User !!!")
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,7 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
